ae8b8fd91286e5bbaecef7489e6483a6a14922eb
rikki1203 <66052659+rikki1203@users.noreply.github.com>
2020-05-29 20:56:16 +0530

    Add files via upload
    Sum of three numbers

---
+++ threenumbersum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    int x,y,z;
+    printf("Enter three numbers:\n");
+    scanf("%d%d%d",&x,&y,&z);
+    printf("Sum of %d+%d+%d is %d\n",x,y,z,x+y+z);
+    return 0;
+}
